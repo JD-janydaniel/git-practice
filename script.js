1cdc223a9894673b2e86d1242f72d9979b78a5b9
JD-janydaniel <janydaniel59@gmail.com>
2024-01-28 21:09:45 +0530

    modified and uploaded

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//print all 250 countries capital and area
+//In here why we are using if else statement
+//in the rescountries API there is now catital for some countries
 var request = new XMLHttpRequest();
 request.open("GET","https://restcountries.com/v3.1/all")
 request.send();
@@ -5,6 +8,22 @@ request.onload = function(){
     var res = JSON.parse(request.response);
     console.log(res);
     for(var i=0;i<res.length;i++){
+        if(res[i].capital){
     console.log(res[i].capital[0],res[i].area);
 }
+else{
+    console.log("Invalid capital",res[i].area);
 }
+}
+}
+//print all 250 common name
+/*var request = new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v3.1/all")
+request.send();
+request.onload = function(){
+    var res = JSON.parse(request.response);
+    console.log(res);
+    for(var i=0;i<res.length;i++){
+        console.log(res[i].name.common);
+    }
+}*/
